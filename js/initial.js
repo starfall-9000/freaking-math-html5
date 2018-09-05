0f5fb12edb08529d25b7a6117979acc4ce3182f9
starfall_9000 <starfall.9000.21@gmail.com>
2018-09-05 11:27:41 +0700

    integrate fb instant

--- js/initial.js
+++ js/initial.js
@@ -1,12 +1,24 @@
-$(document).ready(function() {
-  // need to setup enviroment
+if (gameEnv === 'DEV') {
+  $(document).ready(function() {
+    setupGame()
+  })
+} else {
+  FBInstant.initializeAsync().then(function() {
+    FBInstant.setLoadingProgress(100)
+    FBInstant.startGameAsync().then(function() {
+      setupGame()
+    })
+  })
+}
+
+function setupGame() {
   // app open
   initGame()
   syncPlayer()
   getPlayerLeaderboard()
   // subscribeGame()
   checkIsChallenge()
-})
+}
 
 function initGame() {
   resetScore()
